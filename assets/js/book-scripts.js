747661bcc3d603061d9a00f2877ce3139703b582
Derek <derek@thinkific.com>
2020-04-26 22:11:46 -0600

    feat(ftb-navigation): drawer behaviour complete

--- assets/js/book-scripts.js
+++ assets/js/book-scripts.js
@@ -1,1 +1,16 @@
-alert('fukk you')
+/* globals $ */
+// TODO: convert these to es5
+
+$(() => {
+  $('.remove-if-js').remove();
+  $('.visible-if-js').show();
+
+  $('.book-drawer__toggle').click(() => {
+    // toggle drawer visibility class
+    $('.book-drawer__container').toggleClass('book-drawer__container--visible');
+  });
+
+  $('.book-drawer__container a').click(() => {
+    $('.book-drawer__container').toggleClass('book-drawer__container--visible');
+  });
+});
