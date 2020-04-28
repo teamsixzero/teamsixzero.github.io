e19196605c2fbaa9ed03a562c986860662cb4310
Derek <derek@thinkific.com>
2020-04-28 01:18:20 -0600

    feat(ebook--ftb-navigation): enable content-click-to-close-drawer

--- assets/js/book-scripts.js
+++ assets/js/book-scripts.js
@@ -2,15 +2,44 @@
 // TODO: convert these to es5
 
 $(() => {
+  const $drawerContainer = $('.book-drawer__container');
+  const $drawerContentOverlay = $('.book-drawer__content-overlay');
+  const drawerVisibleClass = 'book-drawer__container--visible';
+
+  // helpers
+  // ----------------------------------
+  const hideDrawer = () => {
+    $drawerContainer.removeClass('book-drawer__container--visible');
+    $drawerContentOverlay.hide();
+  };
+
+  const toggleDrawer = () => {
+    $drawerContainer.toggleClass(drawerVisibleClass);
+
+    if ($drawerContainer.hasClass(drawerVisibleClass)) {
+      $drawerContentOverlay.show();
+    } else {
+      $drawerContentOverlay.hide();
+    }
+  };
+
+  // initializers
+  // -----------------------------------
   $('.remove-if-js').remove();
   $('.visible-if-js').show();
 
+  // listeners
+  // --------------------------------------------
   $('.book-drawer__toggle').click(() => {
-    // toggle drawer visibility class
-    $('.book-drawer__container').toggleClass('book-drawer__container--visible');
+    toggleDrawer();
   });
 
+  $('.book-drawer__content-overlay').click(() => {
+    hideDrawer();
+  });
+
+  // Close drawer when an anchor tag is clicked
   $('.book-drawer__container a').click(() => {
-    $('.book-drawer__container').toggleClass('book-drawer__container--visible');
+    hideDrawer();
   });
 });
